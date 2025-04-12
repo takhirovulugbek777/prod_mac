cee95f24921316efd2d551dc87d199b403efa1e9
takhirov <ulugbekt566@gmail.com>
2025-04-12 23:48:45 +0500

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ sed -i "s/POSTGRES_HOST=localhost/POSTGRES_HOST=db/g" .env
 
 # Start Django with Gunicorn in the background
 echo "Starting Django with Gunicorn..."
-gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3 --daemon
+gunicorn config.wsgi:application --bind 0.0.0.0:80 --workers 3 --daemon
 
 # Wait for Django to start up
 echo "Waiting for Django to start up..."
