dd5eb57e1b24d360079bf4185a4b72fa2a1fc30c
takhirov <ulugbekt566@gmail.com>
2025-04-14 16:55:26 +0500

    nginx fix static

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
 # Django'ni Gunicorn orqali foregroundda ishga tushiramiz
 echo "Starting Django with Gunicorn..."
 gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3 &
