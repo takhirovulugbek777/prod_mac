cd7ff2b600ebc0fcba7ee56c5092ef66b83ef037
takhirov <ulugbekt566@gmail.com>
2025-04-15 17:25:25 +0500

    nginx fix static

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,21 @@
 FROM python:3.11-slim
 
-# Kerakli tizim kutubxonalarini o‘rnatish
 RUN apt-get update && apt-get install -y gcc libpq-dev
 
-# Muhit o‘zgaruvchilari
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
 
-# Ishchi papkani sozlash
 WORKDIR /app
 
-# Kutubxonalarni o‘rnatish
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 RUN pip install gunicorn
 
-# Kod fayllarini nusxalash
 COPY . .
 
-# Portni ochish (endi 8000-da ishlaydi)
 EXPOSE 8000
 
-# Kirish faylini qo‘shish
 COPY ./entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
-# Kirish fayli orqali ishga tushirish
 ENTRYPOINT ["/entrypoint.sh"]
--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,11 @@ DEBUG = False
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 ALLOWED_HOSTS = ["*"]
 
+CSRF_COOKIE_SECURE = True  # faqat HTTPS bo'lsa
+SESSION_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
 INSTALLED_APPS = [
     'jazzmin',
     # "unfold",
@@ -42,6 +47,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
 ]
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -86,7 +92,6 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
-
 
 # DATABASES = {
 #     'default': {
@@ -136,12 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
